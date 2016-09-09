c707bc5dc2ac0c0cfd15d029f64f22e46b88550e
Sanju Thomas <sanju@sanju.org>
2016-09-09 15:31:24 -0400

    Update WebLogReader.java

--- src/main/java/org/sanju/log/reader/WebLogReader.java
+++ src/main/java/org/sanju/log/reader/WebLogReader.java
@@ -12,20 +12,20 @@ import java.net.URL;
  * @author Sanju Thomas
  */
 
-public class WebLogReader{
+public class WebLogReader {
 
 	public static void main(final String[] args) throws URISyntaxException, IOException, InterruptedException {
-		
-		Authenticator.setDefault(new Authenticator(){
-			protected PasswordAuthentication getPasswordAuthentication(){
+
+		Authenticator.setDefault(new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication("admin", "admin".toCharArray());
 			}
 		});
 
 		String uri = null;
-		if(args.length == 1){
+		if (args.length == 1) {
 			uri = args[0];
-		}else{
+		} else {
 			System.out.println("Usage : org.sanju.log.streamer.WebLogReader <<URL>>");
 			System.exit(0);
 		}
@@ -34,15 +34,16 @@ public class WebLogReader{
 		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new InputStreamReader(uriObject.openStream()));
-			while(true){
+			while (true) {
 				final String line = reader.readLine();
-				if(null == line){
+				if (null == line) {
 					Thread.sleep(1000);
+				} else {
+					System.out.println(line);
 				}
-				System.out.println(line);
 			}
-		} finally{
-			if(null != reader) {
+		} finally {
+			if (null != reader) {
 				reader.close();
 			}
 		}
